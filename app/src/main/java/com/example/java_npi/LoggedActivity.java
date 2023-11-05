680527226df2b78a3dd849b460bf6bb6b3c89101
Alejandro Nieto <alejandronietoalarconpersonal@gmail.com>
2023-11-05 21:18:20 +0100

    First commit

---
+++ app/src/main/java/com/example/java_npi/LoggedActivity.java
@@ -0,0 +1,16 @@
+package com.example.java_npi;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoggedActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logged_activity);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/java_npi/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.java_npi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.SQLOutput;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+
+    Button loginBton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.usernameInfo);
+        password = (EditText) findViewById(R.id.passwordInfo);
+        loginBton = (Button) findViewById(R.id.loginButton);
+
+        loginBton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usrName = String.valueOf(username.getText());
+                String pass = String.valueOf(password.getText());
+
+                //Toast.makeText(getApplicationContext(), usrName, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), LoggedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
