a3d7da7e9a20906aaae1d251a79b244262c7b0ae
Alejandro Nieto <alejandronietoalarconpersonal@gmail.com>
2023-11-10 12:11:19 +0100

    QR Display

--- app/src/main/java/com/example/java_npi/MainActivity.java
+++ app/src/main/java/com/example/java_npi/MainActivity.java
@@ -1,23 +1,30 @@
 package com.example.java_npi;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import android.widget.Toast;
 
 import java.sql.SQLOutput;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     EditText username;
     EditText password;
 
     Button loginBton;
+
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
                 //Toast.makeText(getApplicationContext(), usrName, Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(getApplicationContext(), LoggedActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, usrName+"@"+pass);
+
                 startActivity(intent);
             }
         });
