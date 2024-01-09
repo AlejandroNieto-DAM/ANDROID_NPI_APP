d5090aef83b90d4ee118bee0ba71df9d730db2e1
Li-bear <allisoncastellanos8@gmail.com>
2024-01-09 22:18:30 +0100

    Toolbar added

--- app/src/main/java/com/example/java_npi/MenuActivity.java
+++ app/src/main/java/com/example/java_npi/MenuActivity.java
@@ -2,10 +2,26 @@ package com.example.java_npi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionService;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerResultsIntent;
+import android.speech.SpeechRecognizer;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -17,6 +33,7 @@ public class MenuActivity extends AppCompatActivity {
     LinearLayout administration;
     LinearLayout brujula;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +44,10 @@ public class MenuActivity extends AppCompatActivity {
         administration = (LinearLayout) findViewById(R.id.administration);
         brujula = (LinearLayout) findViewById(R.id.brujula);
 
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        // Set up the Toolbar
+
+        setSupportActionBar(toolbar);
 
         qrOption.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,5 +86,35 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+    }
+    public void onBackButtonClick(View view) {
+        // Perform actions on back button click (e.g., navigate back)
+        onBackPressed();
+    }
+
+    public void onVoiceButtonClick(View view) {
+        // Perform actions on back button click (e.g., navigate back)
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "es-ES"); // Set Spanish language
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Habla ahora");
+        startActivityForResult(intent, 111);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 111 && resultCode == RESULT_OK) {
+            // Obtener la lista de resultados
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            // Mostrar el primer resultado (la transcripción de voz)
+            if (result != null && result.size() > 0) {
+                String transcripcion = result.get(0);
+                // Haz algo con la transcripción, por ejemplo, muéstrala en un Toast
+                Toast.makeText(this, "Texto reconocido: " + transcripcion, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
