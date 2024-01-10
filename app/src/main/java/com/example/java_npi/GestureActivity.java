0c25d4d15c9fd1ab46761283bbd84597cd5cca7f
Alejandro Nieto <alejandronietoalarconpersonal@gmail.com>
2024-01-10 23:42:35 +0100

    Cambio layout menu e implementación de gestos en el menu

--- app/src/main/java/com/example/java_npi/GestureActivity.java
+++ app/src/main/java/com/example/java_npi/GestureActivity.java
@@ -11,23 +11,40 @@ import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
 import android.gesture.GestureOverlayView;
 import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class GestureActivity extends AppCompatActivity implements OnGesturePerformedListener {
 
     private GestureLibrary objGestureList;
 
+    Button prueba;
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gesture_activity);
+
+        prueba = (Button) findViewById(R.id.btn_prueba);
 
         objGestureList = GestureLibraries.fromRawResource(this, R.raw.gesture);
         if(!objGestureList.load()){
             finish();
         }
 
+        prueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Funciona");
+            }
+        });
+
         GestureOverlayView objGestureOverlay = (GestureOverlayView) findViewById(R.id.gesture_layout);
         objGestureOverlay.addOnGesturePerformedListener(this);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/com/example/java_npi/MenuActivity.java
+++ app/src/main/java/com/example/java_npi/MenuActivity.java
@@ -1,31 +1,42 @@
 package com.example.java_npi;
 
 import android.content.Intent;
+import android.gesture.Gesture;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MenuActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    LinearLayout qrOption;
+public class MenuActivity extends AppCompatActivity implements OnGesturePerformedListener  {
 
-    LinearLayout locationsOption;
+    private GestureLibrary objGestureList;
 
-
-    LinearLayout administration;
-    LinearLayout brujula;
-
-    LinearLayout reservar_menu;
-
-    LinearLayout gestos;
+    LinearLayout qrOption, locationsOption, administration, brujula, reservar_menu, gestos;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+
+        objGestureList = GestureLibraries.fromRawResource(this, R.raw.gesture);
+        if(!objGestureList.load()){
+            finish();
+        }
+
+        GestureOverlayView objGestureOverlay = (GestureOverlayView) findViewById(R.id.gesture_layout);
+        objGestureOverlay.addOnGesturePerformedListener(this);
 
         qrOption = (LinearLayout) findViewById(R.id.qr_option);
         locationsOption = (LinearLayout) findViewById(R.id.locations_option);
@@ -88,5 +99,19 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+        ArrayList<Prediction> objPrediction = objGestureList.recognize(gesture);
+        if(objPrediction.size() > 0 && objPrediction.get(0).score > 1){
+            String gestureName = objPrediction.get(0).name;
+            Toast.makeText(this, gestureName, Toast.LENGTH_SHORT).show();
+            if (gestureName.contains("Daltonismo")){
+                String a = "Sisi";
+                Toast.makeText(this, "Changing to daltonic mode", Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
     }
 }
